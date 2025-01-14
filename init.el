ef3934effdb9feab2dabef0624ae028e1d987e72
manjie <297867870@qq.com>
2025-01-15 03:52:58 +0800

    0.2

--- init.el
+++ init.el
@@ -30,28 +30,6 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Enable Evil
-(use-package evil
-  :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-C-i-jump nil)
-  (setq evil-want-C-u-scroll t)
-  :ensure t
-  :config
-  (evil-mode 1)    ;; 启用 Evil
-  ;; Use visual line motions even outside of visual-line-mode buffers
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-
-  (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal))                      
-
-(use-package evil-collection
-:after evil
-:config
-(evil-collection-init))
-
 (load-theme 'gruvbox-dark-soft t)
 
 ;; Automatically tangle our Emacs.org config file when we save it
@@ -75,14 +53,6 @@
 (use-package org
 :hook (org-mode . manjie/org-mode-setup))
 
-(use-package company
-:ensure t
-:config
-(global-company-mode 1)
-;; company mode 默认选择上一条和下一条候选项命令 M-n M-p
-:bind (("C-n" . company-select-next)
-       ("C-p" . company-select-previous)))
-
 (use-package helpful
   :ensure t
   ;; 快捷键绑定
@@ -91,16 +61,45 @@
 	     ("C-h k" . helpful-key)
 	     ("C-h x" . helpful-command)))
 
-(tab-bar-mode -1)
+(tab-bar-mode 1)
 
 (use-package ace-jump-mode
   :ensure t
   :bind
   (("C-c SPC" . ace-jump-mode)) ;; 可以绑定到您喜欢的快捷键
   :config
+  (ace-jump-mode 1)
   ;; 如果使用 evil 模式，可以绑定到 normal 模式下的快捷键
   (with-eval-after-load 'evil
-    (define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode)))
+    (define-key evil-normal-state-map (kbd "SPC SPC") 'ace-jump-mode)))
+
+;; Enable Evil
+  (use-package evil
+    :init
+    (setq evil-want-integration t)
+    (setq evil-want-keybinding nil)
+    (setq evil-want-C-i-jump nil)
+    (setq evil-want-C-u-scroll t)
+    :ensure t
+    :config
+    (evil-mode 1)    ;; 启用 Evil
+    ;; Use visual line motions even outside of visual-line-mode buffers
+    (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+    (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+    (evil-set-initial-state 'messages-buffer-mode 'normal)
+    (evil-set-initial-state 'dashboard-mode 'normal))                      
+
+  (use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+  ;; evil-nerd-commenter: 快速注释代码
+(use-package evil-nerd-commenter
+  :ensure t
+  :bind
+  ("M-/" . evilnc-comment-or-uncomment-lines)) ;; 绑定注释快捷键
 
 ;; 安装和配置 Helm
 (use-package helm
@@ -119,3 +118,83 @@
          ("C-x b" . helm-mini)                ;; 切换缓冲区
          ("M-y" . helm-show-kill-ring)        ;; 剪贴板历史
          ("C-c h o" . helm-occur)))           ;; 搜索当前缓冲区内容
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :config
+  (setq doom-modeline-icon nil))
+
+;; undo-tree 配置
+(use-package undo-tree
+  :ensure t
+  :init
+  (setq undo-tree-auto-save-history t) ;; 自动保存 undo-tree 的历史记录
+  (setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo"))) ;; 设置历史记录存储位置
+  (setq undo-tree-visualizer-timestamps t) ;; 在可视化器中显示时间戳
+  (setq undo-tree-visualizer-diff t) ;; 在可视化器中显示 diff
+  :config
+  (global-undo-tree-mode 1) ;; 启用全局 undo-tree
+  ;; 与 evil 模式集成
+  (with-eval-after-load 'evil
+    (define-key evil-normal-state-map (kbd "u") 'undo-tree-undo) ;; 撤销
+    (define-key evil-normal-state-map (kbd "C-r") 'undo-tree-redo))) ;; 重做
+
+(use-package company
+:ensure t
+:config
+(global-company-mode 1)
+;; company mode 默认选择上一条和下一条候选项命令 M-n M-p
+:bind (("C-n" . company-select-next)
+       ("C-p" . company-select-previous)))
+
+;;helm-gtags?
+
+;;  (defun my/update-gtags ()
+;;  "Update GTAGS in the project root."
+;;  (when (and (derived-mode-p 'prog-mode) (executable-find "global"))
+;;    (let ((default-directory (locate-dominating-file default-directory "GTAGS")))
+;;      (when default-directory
+;;        (start-process "update-gtags" nil "global" "-u")))))
+;;
+;;(add-hook 'after-save-hook #'my/update-gtags)
+
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (python-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
+;; if you are helm user
+(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+;; if you are ivy user
+;;(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; optionally if you want to use debugger
+(use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+;; optional if you want which-key integration
+(use-package which-key
+    :config
+    (which-key-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(which-key dap-mode lsp-treemacs helm-lsp lsp-ui lsp-mode vterm vertico undo-tree helpful helm-gtags gruvbox-theme evil-nerd-commenter evil-collection doom-modeline consult-flycheck company ace-jump-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
